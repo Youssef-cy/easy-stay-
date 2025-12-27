e71913c8f69ab611391651b376fcc07813d95a9e
RodinaMohamed2009 <rhsn6786@gmail.com>
2025-12-27 13:26:54 +0200

    first commit

---
+++ java/java.js
@@ -0,0 +1,17 @@
+const img = ["/imgPicture3.png", "/imgPicture1.jpeg", "/img/Picture2.jpeg","img/Picture5.png","img/Picture6.png","img/Picture7.png"];
+    let index = 0;
+    const imge = document.getElementById("changingImage");
+
+    function changeImage() {
+
+        img.style.opacity = 0;
+
+        setTimeout(() => {
+            index++;
+            if(index >= images.length) index = 0;
+            imge.src = images[index];
+            imge.style.opacity = 1;
+        }, 500); 
+    }
+
+    setInterval(changeImage, 3000);
