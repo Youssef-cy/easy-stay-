3514289184977c976051e9aa076562e78a15ef42
RodinaMohamed2009 <rhsn6786@gmail.com>
2025-12-31 17:56:38 +0200

    first commit

---
+++ Transactions.sql
@@ -0,0 +1,84 @@
+CREATE TABLE transactions (
+  transactions_id NUMBER PRIMARY KEY,
+  amount NUMBER(12,2) NOT NULL
+);
+
+
+CREATE SEQUENCE transactions_seq
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+NOCACHE
+NOCYCLE;
+
+
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 450.75);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 1299.99);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 2750.50);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 3890.00);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 5120.25);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 640.80);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 7999.99);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 2150.00);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 9600.40);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 3325.60);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 15000.00);
+INSERT INTO transactions VALUES (transactions_seq.NEXTVAL, 875.30);
+
+
+CREATE OR REPLACE FUNCTION get_total_revenue
+RETURN NUMBER
+IS
+  total_revenue NUMBER;
+BEGIN
+  SELECT SUM(amount)
+  INTO total_revenue
+  FROM transactions;
+
+  RETURN total_revenue;
+END;
+
+
+BEGIN
+  DBMS_OUTPUT.PUT_LINE(get_total_revenue);
+END;
+
+
+CREATE OR REPLACE PROCEDURE add_transaction (p_amount IN NUMBER) IS
+BEGIN
+    INSERT INTO transactions (transactions_id, amount)
+    VALUES (transactions_seq.NEXTVAL, p_amount);
+    
+EXCEPTION
+    WHEN VALUE_ERROR THEN
+     DBMS_OUTPUT.PUT_LINE('Invalid amount');
+    WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE(SQLERRM);
+       
+END;
+
+BEGIN
+    add_transaction(500);
+END;
+
+
+SELECT * FROM reservations
+WHERE booking_cost =(SELECT MAX(booking_cost)
+FROM reservations
+);
+    
+    
+    
+    
+SELECT 
+u.user_id,u.user_name,
+COUNT (r.reservation_id)AS reservation_count
+FROM users u
+LEFT JOIN reservations r
+ON u.user_id = r.user_id
+GROUP BY 
+u.user_id,
+u.user_name
+ORDER BY 
+u.user_id;
+
